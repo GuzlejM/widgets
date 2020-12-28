0e0357c02979567b0bc132b522b6214727775a03
guzlej <martin.guzlej@gmail.com>
2020-12-28 20:50:34 +0000

    downloaded

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Accordion from './components/Accordion';
 const items = [
   {
     title: 'What is React?',
-    content: 'React is a front end javascript framework'
+    content: 'React is a front end javascript framework',
   },
   {
-    title: 'Why use React',
-    content: 'React is a favorite JS library among engineers'
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
   },
   {
     title: 'How do you use React?',
-    content: 'You use React by creating componetns'
-  }
-]
+    content: 'You use React by creating components',
+  },
+];
 
-export default () => { 
-  return(
+export default () => {
+  return (
     <div>
-      <Accordion items={items}/>
+      <Accordion items={items} />
     </div>
-  )
-}
+  );
+};
